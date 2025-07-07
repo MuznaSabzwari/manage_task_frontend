8b091c2c2f7aff28799af8022487fe315fc51f30
SidAli0110 <sidraali01102000@gmail.com>
2025-07-07 19:42:00 +0500

    live issue resolve

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,2 +1,2 @@
-const API = "https://3069f119-a5b4-46f0-b46f-5d4657b72db5-00-1ngqfcc5eunve.sisko.replit.dev/";
+const API = "https://3069f119-a5b4-46f0-b46f-5d4657b72db5-00-1ngqfcc5eunve.sisko.replit.dev";
 export default API;
